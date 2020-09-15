33fa8d960cce71900a957b0348840642643d5bd3
jorant2023 <69231068+jorant2023@users.noreply.github.com>
2020-09-15 13:40:06 -0500

    Create snippets.py

---
+++ snippets.py
@@ -0,0 +1,10 @@
+#Code for filter strings in dataframe
+
+is_30 =  df_training_dataset['Telefono']=='Iphone'
+print('--------------')
+gapminder_2002 = df_training_dataset[is_30]
+print(gapminder_2002.shape)
+print('--------------')
+gapminder_2002.head(20)
+
+#___________________________________________________
