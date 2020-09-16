49888d9448bf077fa079c8970a62da04b4e12593
jorant2023 <69231068+jorant2023@users.noreply.github.com>
2020-09-15 19:52:50 -0500

    Update snippets.py

--- snippets.py
+++ snippets.py
@@ -8,4 +8,47 @@ print('--------------')
 gapminder_2002.head(20)
 
 #___________________________________________________
-#testing
+#contar valores de una columna en el df
+print(df_training_dataset['Genero'].value_counts(),'\n')
+print("Nulos: ",df_training_dataset['Genero'].isna().sum())
+#________________________________________________
+
+#Imputaciones a nulos
+
+#Imputacion constante a -1
+si_constant_minusone = SimpleImputer(
+    missing_values=np.nan,
+    strategy='constant',
+    fill_value=-1,
+    copy=True
+)
+#Imputacion constante a 0
+si_constant_zero = SimpleImputer(
+    missing_values=np.nan,
+    strategy='constant',
+    fill_value=0,
+    copy=True
+)
+#Imputacion mean (sin uso en este caso)
+si_mean = SimpleImputer(
+    missing_values=np.nan,
+    strategy='mean'
+)
+#Imputacion median
+si_median = SimpleImputer(
+    missing_values=np.nan,
+    strategy='median'
+)
+#Imputacion most_frequent (sin uso en este caso)
+si_most_frequent = SimpleImputer(
+    missing_values=np.nan,
+    strategy='most_frequent'
+)
+#____________________________________________________
+#hallar medianas de un df
+df_training_dataset.median(axis = 0) 
+#_____________________________________________
+
+#aumentar tamaño de muestreo en el notebook
+pd.options.display.max_rows = 4000
+#______________________________________________
